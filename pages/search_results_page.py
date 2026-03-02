e564e987103ce5d8ecb990955621484acc537600
Olga350 <gpv260783@gmail.com>
2026-03-02 19:11:47 +0300

    "lesson 7"

--- pages/search_results_page.py
+++ pages/search_results_page.py
@@ -6,8 +6,22 @@ class SearchResultsPage(BasePage):
     #Locators
     @property
     def results_title(self) -> Locator:
-        return self.page. get_by_text('Результаты поиска')
+        return self.page.get_by_text('Результаты поиска')
+    
+    @property
+    def books(self) -> Locator:
+        return self.page.get_by_test_id('art__wrapper')
+    
+    @property
+    def russian_filter(self) ->Locator:
+        return self.page.check('label[for="languages-ru"]')
+    
+    @property
+    def russian_chip(self) -> Locator:
+        return self.page.locator("[data-testid='chip-content']:has-text('Русский')")
 
 
 
     #Actions
+    def apply_russian_filter(self) -> None:
+        self.russian_filter.check()
